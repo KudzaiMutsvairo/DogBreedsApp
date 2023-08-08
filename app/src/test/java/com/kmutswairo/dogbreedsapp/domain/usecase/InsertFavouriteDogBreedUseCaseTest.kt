707a6f7f8e165cbc44e2e456979610259b24202e
KudzaiMutsvairo <Kudzie.mutsvairo@gmail.com>
2023-08-08 20:42:04 +0200

    added InsertFavouriteDogBreedUseCase and tests

---
+++ app/src/test/java/com/kmutswairo/dogbreedsapp/domain/usecase/InsertFavouriteDogBreedUseCaseTest.kt
@@ -0,0 +1,66 @@
+package com.kmutswairo.dogbreedsapp.domain.usecase
+
+import com.kmutswairo.dogbreedsapp.domain.model.FavouriteDogBreed
+import com.kmutswairo.dogbreedsapp.domain.repository.FavouriteDogBreedsRepository
+import com.kmutswairo.dogbreedsapp.util.Resource
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class InsertFavouriteDogBreedUseCaseTest {
+    private lateinit var useCase: InsertFavouriteDogBreedUseCase
+    private var repository: FavouriteDogBreedsRepository = mockk<FavouriteDogBreedsRepository>()
+
+    @Before
+    fun setUp() {
+        useCase = InsertFavouriteDogBreedUseCase(repository)
+    }
+
+    @After
+    fun tearDown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `GIVEN insertFavouriteDogBreed is called, WHEN there are favourite dog breeds in the cache, THEN return Success`() =
+        runTest {
+            // Arrange
+            val dogBreed = FavouriteDogBreed(
+                id = 12,
+                name = "Poodle",
+                subBreeds = emptyList(),
+            )
+            coEvery { repository.insertFavouriteDogBreed(dogBreed) } returns Resource.Success(1L)
+
+            // Act
+            val result = useCase(dogBreed)
+
+            // Assert
+            assertTrue(result is Resource.Success)
+        }
+
+    @Test
+    fun `GIVEN insertFavouriteDogBreed returns an error THEN return Error`() = runTest {
+        // Arrange
+        val dogBreed = FavouriteDogBreed(
+            id = 12,
+            name = "Poodle",
+            subBreeds = emptyList(),
+        )
+        val errorMessage = "Error inserting favourite dog breed"
+        coEvery { repository.insertFavouriteDogBreed(dogBreed) } returns Resource.Error(
+            message = errorMessage,
+        )
+
+        // Act
+        val result = useCase(dogBreed)
+
+        // Assert
+        assertTrue(result is Resource.Error)
+    }
+}
