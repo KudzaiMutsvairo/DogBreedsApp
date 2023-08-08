5ee5f6465ce76b3931866925c5392e85dd9be6b2
KudzaiMutsvairo <Kudzie.mutsvairo@gmail.com>
2023-08-08 23:55:38 +0200

    formatting

--- app/src/main/java/com/kmutswairo/dogbreedsapp/domain/usecase/FetchDogBreedsLocalUseCase.kt
+++ app/src/main/java/com/kmutswairo/dogbreedsapp/domain/usecase/FetchDogBreedsLocalUseCase.kt
@@ -4,7 +4,7 @@ import com.kmutswairo.dogbreedsapp.domain.repository.DogBreedsRepository
 import javax.inject.Inject
 
 class FetchDogBreedsLocalUseCase @Inject constructor(
-    private val repository: DogBreedsRepository
-){
+    private val repository: DogBreedsRepository,
+) {
     suspend operator fun invoke() = repository.getAllDogBreedsFromCache()
 }
