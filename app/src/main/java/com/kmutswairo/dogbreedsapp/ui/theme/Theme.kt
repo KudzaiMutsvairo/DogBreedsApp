95749db6000025e2d8f4d9723bfab999fbc32d18
KudzaiMutsvairo <Kudzie.mutsvairo@gmail.com>
2023-08-08 21:42:00 +0200

    removed dynamic theming

--- app/src/main/java/com/kmutswairo/dogbreedsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/kmutswairo/dogbreedsapp/ui/theme/Theme.kt
@@ -1,13 +1,10 @@
 package com.kmutswairo.dogbreedsapp.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
@@ -87,14 +84,6 @@ fun DogBreedsAppTheme(
 ) {
     val context = LocalContext.current
     val colors = when {
-        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) -> {
-            if (useDarkTheme) {
-                dynamicDarkColorScheme(context)
-            } else {
-                dynamicLightColorScheme(context)
-            }
-        }
-
         useDarkTheme -> DarkColors
         else -> LightColors
     }
