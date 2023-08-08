3e09231097d7a02651e9b5233d2942c8657b04f4
KudzaiMutsvairo <Kudzie.mutsvairo@gmail.com>
2023-08-08 21:51:45 +0200

    added DogBreedItem component

---
+++ app/src/main/java/com/kmutswairo/dogbreedsapp/presentation/components/DogBreedItem.kt
@@ -0,0 +1,97 @@
+package com.kmutswairo.dogbreedsapp.presentation.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kmutswairo.dogbreedsapp.domain.model.DogBreed
+
+@Composable
+fun DogBreedItem(
+    modifier: Modifier = Modifier,
+    breed: DogBreed,
+    onFavoriteToggle: (DogBreed) -> Unit,
+) {
+    Card(
+        modifier = modifier.padding(8.dp),
+        shape = MaterialTheme.shapes.small,
+    ) {
+        Column(modifier = Modifier.padding(3.dp)) {
+            Row(
+                modifier = Modifier,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .weight(1f),
+                ) {
+                    Text(
+                        text = breed.name,
+                        style = MaterialTheme.typography.titleMedium,
+                    )
+                }
+
+                FavouriteToggleIconButton(
+                    breed = breed,
+                    onFavoriteToggle = onFavoriteToggle,
+                )
+            }
+            if (breed.subBreeds.isNotEmpty()) {
+                LazyRow {
+                    items(breed.subBreeds) { item ->
+                        DogSubBreedItem(subBreed = item)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FavouriteToggleIconButton(breed: DogBreed, onFavoriteToggle: (DogBreed) -> Unit) {
+    val isFavouriteState = rememberUpdatedState(breed.isFavourite)
+
+    IconButton(
+        modifier = Modifier,
+        onClick = { onFavoriteToggle(breed) },
+    ) {
+        Icon(
+            imageVector = if (isFavouriteState.value) Icons.Filled.Star else Icons.Outlined.Star,
+            contentDescription = "Add to Favorites",
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DogBreedItemPreview() {
+    DogBreedItem(
+        breed = DogBreed(
+            name = "Poodle",
+            isFavourite = false,
+            subBreeds = listOf(
+                "Toy Poodle",
+                "Miniature Poodle",
+                "Standard Poodle",
+            ),
+        ),
+        onFavoriteToggle = {},
+    )
+}
