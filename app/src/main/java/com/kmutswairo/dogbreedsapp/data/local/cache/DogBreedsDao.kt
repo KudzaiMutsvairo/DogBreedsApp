3ac95426f300e9eb55897d80e83e7805db7f31fa
KudzaiMutsvairo <Kudzie.mutsvairo@gmail.com>
2023-08-08 21:46:32 +0200

    refactor DogBreedsDao

--- app/src/main/java/com/kmutswairo/dogbreedsapp/data/local/cache/DogBreedsDao.kt
+++ app/src/main/java/com/kmutswairo/dogbreedsapp/data/local/cache/DogBreedsDao.kt
@@ -3,6 +3,7 @@ package com.kmutswairo.dogbreedsapp.data.local.cache
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import com.kmutswairo.dogbreedsapp.domain.model.DogBreed
 import com.kmutswairo.dogbreedsapp.util.Constants
 
 @Dao
@@ -11,11 +12,11 @@ interface DogBreedsDao {
     suspend fun insertDogBreeds(breeds: List<DogBreedsEntity>): List<Long>
 
     @Query(
-        "SELECT dog_breeds.*, favorite_breeds.breedName IS NOT NULL as isFavorite FROM " +
+        "SELECT dog_breeds.*, favorite_breeds.breedName IS NOT NULL as isFavourite FROM " +
             "${Constants.TABLE_DOG_BREEDS} LEFT JOIN ${Constants.TABLE_FAVORITE_BREEDS} " +
-            "ON dog_breeds.breedName = favorite_breeds.breedName",
+            "ON dog_breeds.name = favorite_breeds.breedName",
     )
-    suspend fun getAllDogBreeds(): List<DogBreedsEntity>
+    suspend fun getAllDogBreeds(): List<DogBreed>
 
     @Query("DELETE FROM ${Constants.TABLE_DOG_BREEDS}")
     suspend fun deleteAllDogBreeds(): Int
