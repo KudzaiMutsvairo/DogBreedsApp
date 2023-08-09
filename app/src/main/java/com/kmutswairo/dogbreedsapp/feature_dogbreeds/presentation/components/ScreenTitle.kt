d86ab3b8d103498bb84bbe2e8228b6566ced745d
KudzaiMutsvairo <Kudzie.mutsvairo@gmail.com>
2023-08-09 07:19:35 +0200

    updated ScreenTitle

--- app/src/main/java/com/kmutswairo/dogbreedsapp/feature_dogbreeds/presentation/components/ScreenTitle.kt
+++ app/src/main/java/com/kmutswairo/dogbreedsapp/feature_dogbreeds/presentation/components/ScreenTitle.kt
@@ -1,28 +1,46 @@
 package com.kmutswairo.dogbreedsapp.feature_dogbreeds.presentation.components
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.kmutswairo.dogbreedsapp.R
 
 @Composable
 fun ScreenTitle(
     modifier: Modifier = Modifier,
     title: String,
 ) {
-    Box(
+    Row(
         modifier = modifier
             .fillMaxWidth()
-            .padding(8.dp),
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Start,
     ) {
+        Image(
+            modifier = Modifier.size(34.dp),
+            painter = painterResource(id = R.drawable.paws),
+            contentDescription = "Logo",
+            colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary),
+        )
+        Spacer(modifier = Modifier.width(8.dp))
         Text(
             text = title,
-            style = MaterialTheme.typography.titleLarge,
+            style = MaterialTheme.typography.displaySmall,
         )
     }
 }
