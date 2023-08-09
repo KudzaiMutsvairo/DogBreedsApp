0348399f3593a4f4bcb19b10969bd85f3d95308f
KudzaiMutsvairo <Kudzie.mutsvairo@gmail.com>
2023-08-09 07:16:32 +0200

    changed icons

--- app/src/main/java/com/kmutswairo/dogbreedsapp/feature_dogbreeds/presentation/components/DogBreedItem.kt
+++ app/src/main/java/com/kmutswairo/dogbreedsapp/feature_dogbreeds/presentation/components/DogBreedItem.kt
@@ -7,15 +7,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material.icons.outlined.Star
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -65,15 +64,16 @@ fun DogBreedItem(
 }
 
 @Composable
-fun FavouriteToggleIconButton(breed: DogBreed, onFavoriteToggle: (DogBreed) -> Unit) {
-    val isFavouriteState = rememberUpdatedState(breed.isFavourite)
-
+fun FavouriteToggleIconButton(
+    breed: DogBreed,
+    onFavoriteToggle: (DogBreed) -> Unit,
+) {
     IconButton(
         modifier = Modifier,
         onClick = { onFavoriteToggle(breed) },
     ) {
         Icon(
-            imageVector = if (isFavouriteState.value) Icons.Filled.Star else Icons.Outlined.Star,
+            imageVector = if (breed.isFavourite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
             contentDescription = "Add to Favorites",
         )
     }
